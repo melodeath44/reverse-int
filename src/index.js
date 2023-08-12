9bf08316122a632383797a107a176ae3a56e8e88
Vitaly Parkhamovich <melodeath44@mail.ru>
2023-08-12 21:07:35 +0300

    init: try algoritm test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const num = n.toString();
+  const num1 = num.split(" ");
+  const newNum = num1.reverse();
+const reversNum = newNum.join('')
+return +reversNum;
 }
