7c54088fdab0a7243c1628424f9f029a0d794deb
Vitaly Parkhamovich <melodeath44@mail.ru>
2023-08-12 21:39:31 +0300

    feat: complete algoritm task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+
 module.exports = function reverse (n) {
-  const num = n.toString();
-  const num1 = num.split(" ");
-  const newNum = num1.reverse();
-const reversNum = newNum.join('')
-return +reversNum;
-}
+    if(n > 0){
+        const num = n.toString();
+        const num1 = num.split("");
+        const newNum = num1.reverse();
+        const reversNum = newNum.join('')
+        return reversNum;
+    }
+
+
+  else if (n < 0){
+    const numMinus = -1 * n;
+    const numMinus2 = numMinus.toString();
+    const numMinus3 = numMinus2.split("");
+    const newNum1 = numMinus3.reverse();
+    const reversNum1 = newNum1.join('')
+    return reversNum1;
+  }
+  }
